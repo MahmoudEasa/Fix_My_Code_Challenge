7f64bc3fd0a86f8794122bd889374097bb690889
MahmoudEasa <mu01011422865@gmail.com>
2023-10-15 17:27:13 +0000

    Fix square.py 19

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -11,8 +11,40 @@ class Square():
 
     def __init__(self, width=0, height=0):
         """ Initial Function """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Getter for width """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ Setter for width """
+        if type(value) is not int:
+            raise TypeError("width most be an integer")
+
+        if value <= 0:
+            raise ValueError("width most be greater than 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for width """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ Setter for width """
+        if type(value) is not int:
+            raise TypeError("height most be an integer")
+
+        if value <= 0:
+            raise ValueError("height most be greater than 0")
+
+        self.__height = value
 
     def area_of_my_square(self):
         """ Area of the square """
