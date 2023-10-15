fa60bdd3bc54f00f72f97576490f77926fcd61a1
MahmoudEasa <mu01011422865@gmail.com>
2023-10-15 15:57:44 +0000

    Fix square.py 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+""" My square File """
+
+
 class square():
+    """ Class Square """
 
     width = 0
     height = 0
@@ -14,6 +18,7 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -21,7 +26,6 @@ class square():
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
