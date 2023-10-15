68104658ec38db1b01c60903db218287bdc0238d
MahmoudEasa <mu01011422865@gmail.com>
2023-10-15 17:08:20 +0000

    Fix square.py 18

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -6,22 +6,25 @@
 class Square():
     """ Class Square """
 
+    __width = 0
+    __height = 0
+
     def __init__(self, width=0, height=0):
         """ Initial Function """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area_of_my_square(self):
         """ Area of the square """
-        return (self.width * self.height)
+        return (self.__width * self.__height)
 
     def PermiterOfMySquare(self):
         """ Permiter of the square """
-        return ((self.width * 2) + (self.height * 2))
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """ String Function """
-        return ("{}/{}".format(self.width, self.height))
+        return ("{}/{}".format(self.__width, self.__height))
 
 
 if __name__ == "__main__":
